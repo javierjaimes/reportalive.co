10a65589aca2d32583cb30e602f65af04993d770
Javier Jaimes <sr.pasajero@lanzacohete.co>
2013-11-06 20:21:00 -0500

    setup redis, socket io and first messafe

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require( 'express' )(),
+    server = require( 'http' ).createServer( app ),
+    io = require( 'socket.io' ).listen( server ),
+    redis = require( 'redis' ).createClient();
+
+redis.subscribe( 'r-live' );
+server.listen( 8080 );
+
+app.get( '/', function( req, res ){
+  res.sendFile( __dirname + '/index.html' );
+})
+
+io.sockets.on( 'connection', function( socket ){
+  
+  redis.on( 'message', function( channel, message ){
+    console.log( 'reddis', channel, message );
+
+    socket.emit( 'new_extend', message );
+  })
+
+  socket.emit( 'welcome', { message: 'Hello World!' });
+
+  /*socket.on( 'start', function( data ){
+    console.log( data );
+  })*/
+})
+
