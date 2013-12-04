030f688e0f7081daffa07ceca1c91b5098b6f55d
Javier Jaimes <sr.pasajero@lanzacohete.co>
2013-12-03 19:43:30 -0500

    Connect to redis on heroku

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var redisp = process.env.REDISTOGO_URL || 6379,
-    expressp = process.env.PORT || 8080;
-
-console.log( redisp );
-
 var app = require( 'express' )(),
     server = require( 'http' ).createServer( app ),
-    io = require( 'socket.io' ).listen( server ),
-    redis = require( 'redis' ).createClient( redisp );
+    io = require( 'socket.io' ).listen( server );
+
+//Connect to RedisToGo at Heroku
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
 
 redis.subscribe( 'r-live' );
-server.listen( expressp );
+server.listen( process.env.PORT || 8080 );
 
 app.get( '/', function( req, res ){
   res.sendFile( __dirname + '/index.html' );
 })
 
 io.sockets.on( 'connection', function( socket ){
-  
+
   redis.on( 'message', function( channel, message ){
 
     message = JSON.parse( message );
